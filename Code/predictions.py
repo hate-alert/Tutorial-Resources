ca57c8939d8c4796a0dd44dba0cf229e03a06627
Punyajoy Saha <punyajoysaha1998@gmail.com>
2021-05-31 00:06:31 +0530

    Small typo

--- Code/predictions.py
+++ Code/predictions.py
@@ -133,7 +133,7 @@ class modelPred():
         'arabic':"Hate-speech-CNERG/dehatebert-mono-arabic",
         'english': "Hate-speech-CNERG/dehatebert-mono-english",
         'english_hatexplain':"Hate-speech-CNERG/bert-base-uncased-hatexplain",
-        'french': "Hate-speech-CNERG/dehatebert-mono-english",
+        'french': "Hate-speech-CNERG/dehatebert-mono-french",
         'german': "Hate-speech-CNERG/dehatebert-mono-german",
         'indonesian': "Hate-speech-CNERG/dehatebert-mono-indonesian",
         'polish': "Hate-speech-CNERG/dehatebert-mono-polish",
