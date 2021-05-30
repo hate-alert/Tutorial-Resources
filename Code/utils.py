218a068b0dc1d781f95947de6fc7e8745c2ef51f
Punyajoy Saha <punyajoysaha1998@gmail.com>
2021-05-30 21:33:34 +0530

    Small typo

--- Code/utils.py
+++ Code/utils.py
@@ -12,3 +12,23 @@ def softmax(x):
 
 
 
+class CharVal(object):
+    def __init__(self, char, val):
+        self.char = char
+        self.val = val
+
+    def __str__(self):
+        return self.char
+
+def rgb_to_hex(rgb):
+    return '#%02x%02x%02x' % rgb
+
+
+def color_charvals_lime(s):
+    r = 255-int(s.val*255)
+    color = rgb_to_hex((255, r, r))
+    return 'background-color: %s' % color
+def color_charvals_rationale(s):
+    r = 255-int(s.val*255)
+    color = rgb_to_hex((255, r, r))
+    return 'background-color: %s' % color
